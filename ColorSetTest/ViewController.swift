b7f2aa8cc1a9af21ab795987e5eb6e897fb35de6
Masami <masami@MasaminoMacBook-Pro.local>
2020-09-09 23:32:12 +0900

    initialize commit

---
+++ ColorSetTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorSetTest
+//
+//  Created by Masami on 2020/09/09.
+//  Copyright © 2020 MasamiYamate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
